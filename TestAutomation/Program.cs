df980af26f371764661ef621a3d0298b0a17d475
Jimmie Bruken <jimmie.bruken@student.kyh.se>
2019-11-20 11:05:03 +0100

    projekt start

---
+++ TestAutomation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MongoDB;
+using MongoDB.Driver;
+
+namespace TestAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoCRUD db = new MongoCRUD("Trafikverket");
+            Console.ReadLine();
+        }
+    }
+
+    public class MongoCRUD 
+         
+    {
+        private IMongoDatabase db;
+        
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+
+        }
+
+        
+    }
+}
